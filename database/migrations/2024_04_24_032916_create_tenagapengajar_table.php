2dd7534bd1ca0afde54f028b8729e68d8e62521e
Andika PT <andikatampubolon72@gmail.com>
2024-05-02 15:54:00 +0700

    02/5/24

---
+++ database/migrations/2024_04_24_032916_create_tenagapengajar_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenagapengajar', function (Blueprint $table) {
+            $table->increments('id_tenagapengajar');
+            $table->string('nama_tenagapengajar');
+            $table->string('jabatan');
+            $table->string('nip');
+            $table->string('alamat');
+            $table->string('notelepon');
+            $table->string('gambar_tenagapengajar')->nullable();;
+            $table->string('created_by');
+            $table->string('update_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenagapengajar');
+    }
+};
