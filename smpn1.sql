35605abc0e04e57fb07b6e9f9bf3d58de91a8b0b
Yonathan Purba <149058805+YonathanPurba@users.noreply.github.com>
2024-06-01 20:23:42 +0700

    Update smpn1.sql

--- smpn1.sql
+++ smpn1.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 05, 2024 at 05:10 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Waktu pembuatan: 31 Bulan Mei 2024 pada 08.49
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `smpn1`
+-- Database: `laravel`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `agenda`
+-- Struktur dari tabel `agenda`
 --
 
 CREATE TABLE `agenda` (
@@ -45,7 +45,7 @@ CREATE TABLE `agenda` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `artikel`
+-- Struktur dari tabel `artikel`
 --
 
 CREATE TABLE `artikel` (
@@ -60,10 +60,18 @@ CREATE TABLE `artikel` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data untuk tabel `artikel`
+--
+
+INSERT INTO `artikel` (`id`, `judul`, `slug`, `deskripsi`, `thumbnail`, `user_id`, `kategori_artikel_id`, `created_at`, `updated_at`) VALUES
+(1, 'riwgrawngl', 'riwgrawngl', '<p>nhwfnke</p>\n', 'oDMmSpZIxfutsall3.jpeg', 1, NULL, '2024-05-30 05:27:43', '2024-05-30 05:27:43'),
+(2, 'fdagadgda', 'fdagadgda', '<p><img data-filename=\"futsall6.jpeg\" style=\"width: 259px;\" src=\"/uploads/img/artikel/TfESpLgSd17171230060.png\"><br><br><br><img data-filename=\"futsall6.jpeg\" style=\"width: 259px;\" src=\"/uploads/img/artikel/DAFjZ3tTl17171230061.png\"><br><br></p>\n', 'tWsvJzstufutsall3.jpeg', 1, NULL, '2024-05-30 19:36:46', '2024-05-30 19:36:46');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `banners`
+-- Struktur dari tabel `banners`
 --
 
 CREATE TABLE `banners` (
@@ -78,7 +86,31 @@ CREATE TABLE `banners` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `failed_jobs`
+-- Struktur dari tabel `ekstrakulikuler`
+--
+
+CREATE TABLE `ekstrakulikuler` (
+  `id_ekstrakulikuler` int(10) UNSIGNED NOT NULL,
+  `judul_ekstrakulikuler` varchar(255) NOT NULL,
+  `gambar_ekstrakulikuler` varchar(255) DEFAULT NULL,
+  `deskripsi_ekstrakulikuler` varchar(255) NOT NULL,
+  `created_by` varchar(255) NOT NULL,
+  `update_by` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `ekstrakulikuler`
+--
+
+INSERT INTO `ekstrakulikuler` (`id_ekstrakulikuler`, `judul_ekstrakulikuler`, `gambar_ekstrakulikuler`, `deskripsi_ekstrakulikuler`, `created_by`, `update_by`, `created_at`, `updated_at`) VALUES
+(4, 'gfsg', 'futsall5.jpeg', 'dsafg', '1', '1', '2024-05-29 20:18:24', '2024-05-29 20:18:24');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `failed_jobs`
 --
 
 CREATE TABLE `failed_jobs` (
@@ -94,7 +126,34 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kategori_artikel`
+-- Struktur dari tabel `fasilitas`
+--
+
+CREATE TABLE `fasilitas` (
+  `id_fasilitas` int(10) UNSIGNED NOT NULL,
+  `nama_fasilitas` varchar(255) NOT NULL,
+  `gambar_fasilitas` varchar(255) DEFAULT NULL,
+  `deskripsi_fasilitas` varchar(255) NOT NULL,
+  `created_by` varchar(255) NOT NULL,
+  `update_by` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `fasilitas`
+--
+
+INSERT INTO `fasilitas` (`id_fasilitas`, `nama_fasilitas`, `gambar_fasilitas`, `deskripsi_fasilitas`, `created_by`, `update_by`, `created_at`, `updated_at`) VALUES
+(5, 'dsagds', 'futsall5.jpeg', 'gdsgs', '1', '1', '2024-05-29 01:47:11', '2024-05-29 19:44:55'),
+(6, 'Ruang Kelas', 'futsall5.jpeg', 'gfdgd', '1', '1', '2024-05-29 01:48:09', '2024-05-29 01:48:09'),
+(7, 'gfgfsg', 'image (9).png', 'ddadf', '1', '1', '2024-05-29 05:52:51', '2024-05-29 05:52:51'),
+(8, 'xdfxf', 'futsall5.jpeg', 'cfccfcf', '1', '1', '2024-05-30 19:42:02', '2024-05-30 19:42:02');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kategori_artikel`
 --
 
 CREATE TABLE `kategori_artikel` (
@@ -105,17 +164,10 @@ CREATE TABLE `kategori_artikel` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Dumping data for table `kategori_artikel`
---
-
-INSERT INTO `kategori_artikel` (`id`, `nama_kategori`, `slug`, `created_at`, `updated_at`) VALUES
-(3, 'Contoh', 'contoh', '2024-04-22 01:30:06', '2024-04-22 01:30:06');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `migrations`
+-- Struktur dari tabel `migrations`
 --
 
 CREATE TABLE `migrations` (
@@ -125,25 +177,31 @@ CREATE TABLE `migrations` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `migrations`
+-- Dumping data untuk tabel `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2014_10_12_200000_add_two_factor_columns_to_users_table', 1),
-(4, '2019_08_19_000000_create_failed_jobs_table', 1),
-(5, '2021_03_11_230713_create_pengumuman_table', 1),
-(6, '2021_03_11_230723_create_agenda_table', 1),
-(7, '2021_03_12_170942_create_artikel_table', 1),
-(8, '2021_03_12_170959_create_kategori_artikel_table', 1),
-(9, '2021_03_12_171751_create_banners_table', 1),
-(10, '2021_03_12_172043_create_navbars_table', 1);
+(28, '2024_04_22_084001_create_tenagapengajar_table', 2),
+(164, '2021_03_11_230723_create_agenda_table', 3),
+(172, '2014_10_12_000000_create_users_table', 4),
+(173, '2014_10_12_100000_create_password_resets_table', 4),
+(174, '2014_10_12_200000_add_two_factor_columns_to_users_table', 4),
+(175, '2019_08_19_000000_create_failed_jobs_table', 4),
+(176, '2019_12_14_000001_create_personal_access_tokens_table', 4),
+(177, '2021_03_11_230713_create_pengumuman_table', 4),
+(178, '2021_03_12_170942_create_artikel_table', 4),
+(179, '2021_03_12_170959_create_kategori_artikel_table', 4),
+(180, '2021_03_12_171751_create_banners_table', 4),
+(181, '2021_03_12_172043_create_navbars_table', 4),
+(182, '2024_04_24_032916_create_tenagapengajar_table', 4),
+(183, '2024_05_10_015040_create_fasilitas_table', 4),
+(184, '2024_05_13_131135_create_prestasi_table', 4),
+(185, '2024_05_29_013607_create_ekstrakulikuler_table', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `navbars`
+-- Struktur dari tabel `navbars`
 --
 
 CREATE TABLE `navbars` (
@@ -158,7 +216,7 @@ CREATE TABLE `navbars` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `password_resets`
+-- Struktur dari tabel `password_resets`
 --
 
 CREATE TABLE `password_resets` (
@@ -170,7 +228,7 @@ CREATE TABLE `password_resets` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengumuman`
+-- Struktur dari tabel `pengumuman`
 --
 
 CREATE TABLE `pengumuman` (
@@ -185,16 +243,82 @@ CREATE TABLE `pengumuman` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `pengumuman`
+-- Dumping data untuk tabel `pengumuman`
 --
 
 INSERT INTO `pengumuman` (`id`, `judul`, `deskripsi`, `tgl`, `slug`, `user_id`, `created_at`, `updated_at`) VALUES
-(3, 'Contoh', '<p>123</p>', '2024-04-25', 'contoh', 3, '2024-04-25 01:34:22', '2024-04-25 01:34:22');
+(1, 'fdafds', '<p>gfs</p>', '2024-05-30', 'fdafds', 1, '2024-05-29 19:53:39', '2024-05-29 19:54:34');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `prestasi`
+--
+
+CREATE TABLE `prestasi` (
+  `id_prestasi` int(10) UNSIGNED NOT NULL,
+  `judul_prestasi` varchar(255) NOT NULL,
+  `gambar_prestasi` varchar(255) DEFAULT NULL,
+  `deskripsi_prestasi` varchar(255) NOT NULL,
+  `tanggal_prestasi` date NOT NULL,
+  `created_by` varchar(255) NOT NULL,
+  `update_by` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tenagapengajar`
+--
+
+CREATE TABLE `tenagapengajar` (
+  `id_tenagapengajar` int(10) UNSIGNED NOT NULL,
+  `nama_tenagapengajar` varchar(255) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `nip` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `notelepon` varchar(255) NOT NULL,
+  `gambar_tenagapengajar` varchar(255) DEFAULT NULL,
+  `created_by` varchar(255) NOT NULL,
+  `update_by` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `tenagapengajar`
+--
+
+INSERT INTO `tenagapengajar` (`id_tenagapengajar`, `nama_tenagapengajar`, `jabatan`, `nip`, `alamat`, `notelepon`, `gambar_tenagapengajar`, `created_by`, `update_by`, `created_at`, `updated_at`) VALUES
+(2, 'tgdhr', 'nhfmr', 'nrhrnr', 'reye', '321341', 'futsall3.jpeg', '1', '1', '2024-05-29 20:51:11', '2024-05-29 20:51:11'),
+(3, '434214', 'daag', '4143q2', 'gfshsf', 'dag', 'futsall5.jpeg', '1', '1', '2024-05-29 20:52:15', '2024-05-29 20:52:15'),
+(4, 'ytjee', '5htrhs', '432432', 'henerner', '3131213', 'futsall2.jpeg', '1', '1', '2024-05-30 00:34:17', '2024-05-30 00:34:17');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -211,137 +335,200 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `remember_token`, `created_at`, `updated_at`) VALUES
-(3, 'Yonathan Purba', 'yonathanpurba@yahoo.com', NULL, '$2y$10$H250u3SChrADlaYW4AxgAeHxQ9AZcG70V3oxmmY8yHU//3SxK/Sbi', NULL, NULL, NULL, NULL, '2024-04-23 16:14:16'),
-(1232, 'contoh', 'contoh@gmail.com', NULL, '$2y$10$CRJ3sfmMCOo3yh00nOzZguZay9yzov7tSwGGpURGTW1b7wSyxgVAW', NULL, NULL, NULL, '2024-04-10 04:43:56', '2024-04-10 04:43:56'),
-(1240, 'YONATHAN', 'YONATHAN@YAHOO.COM', NULL, '$2y$10$AsvIz/wfzypyne/Nznh7neYN1pkV0QvGemi3qs2tdQTBGicxRi7mG', NULL, NULL, NULL, '2024-04-25 01:33:47', '2024-04-25 01:33:47');
+(1, 'Andika', 'andikatampubolon72@gmail.com', NULL, '$2y$10$xKZMbbjvtXkTNnUyofFm4Oex2gbEiO2HsAys.WPjgi916tbYVz06G', NULL, NULL, NULL, NULL, NULL),
+(2, 'dsafa', 'andikatampubolon723@gmail.com', NULL, '$2y$10$1pHzj0y2Lv2L8/Bg6AAPoOJUXAe7KWgIMO7fK.cPRce2wVnXjmrsm', NULL, NULL, NULL, '2024-05-29 01:37:02', '2024-05-29 01:37:02'),
+(3, 'fdsgs', 'andikatampubolon74@gmail.com', NULL, '$2y$10$vsKQ9JJkdq3lkWRKGY1ZEerzH4TvBpBeOwXMf2KTFs3b/8a0OeXLO', NULL, NULL, NULL, '2024-05-29 01:39:26', '2024-05-29 01:39:26'),
+(4, 'gfsg', 'andikatampubolon754@gmail.com', NULL, '$2y$10$X9of9unCPHbI1/ujqPk70ulF4t5ELJJon4eq62sBvRB5V435fl1vG', NULL, NULL, NULL, '2024-05-29 01:42:29', '2024-05-29 01:42:29');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `agenda`
+-- Indeks untuk tabel `agenda`
 --
 ALTER TABLE `agenda`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `artikel`
+-- Indeks untuk tabel `artikel`
 --
 ALTER TABLE `artikel`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `banners`
+-- Indeks untuk tabel `banners`
 --
 ALTER TABLE `banners`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `failed_jobs`
+-- Indeks untuk tabel `ekstrakulikuler`
+--
+ALTER TABLE `ekstrakulikuler`
+  ADD PRIMARY KEY (`id_ekstrakulikuler`);
+
+--
+-- Indeks untuk tabel `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
--- Indexes for table `kategori_artikel`
+-- Indeks untuk tabel `fasilitas`
+--
+ALTER TABLE `fasilitas`
+  ADD PRIMARY KEY (`id_fasilitas`);
+
+--
+-- Indeks untuk tabel `kategori_artikel`
 --
 ALTER TABLE `kategori_artikel`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `migrations`
+-- Indeks untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `navbars`
+-- Indeks untuk tabel `navbars`
 --
 ALTER TABLE `navbars`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `password_resets`
+-- Indeks untuk tabel `password_resets`
 --
 ALTER TABLE `password_resets`
   ADD KEY `password_resets_email_index` (`email`);
 
 --
--- Indexes for table `pengumuman`
+-- Indeks untuk tabel `pengumuman`
 --
 ALTER TABLE `pengumuman`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indeks untuk tabel `prestasi`
+--
+ALTER TABLE `prestasi`
+  ADD PRIMARY KEY (`id_prestasi`);
+
+--
+-- Indeks untuk tabel `tenagapengajar`
+--
+ALTER TABLE `tenagapengajar`
+  ADD PRIMARY KEY (`id_tenagapengajar`);
+
+--
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_unique` (`email`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `agenda`
+-- AUTO_INCREMENT untuk tabel `agenda`
 --
 ALTER TABLE `agenda`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `artikel`
+-- AUTO_INCREMENT untuk tabel `artikel`
 --
 ALTER TABLE `artikel`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `banners`
+-- AUTO_INCREMENT untuk tabel `banners`
 --
 ALTER TABLE `banners`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `failed_jobs`
+-- AUTO_INCREMENT untuk tabel `ekstrakulikuler`
+--
+ALTER TABLE `ekstrakulikuler`
+  MODIFY `id_ekstrakulikuler` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `kategori_artikel`
+-- AUTO_INCREMENT untuk tabel `fasilitas`
+--
+ALTER TABLE `fasilitas`
+  MODIFY `id_fasilitas` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `kategori_artikel`
 --
 ALTER TABLE `kategori_artikel`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `migrations`
+-- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=186;
 
 --
--- AUTO_INCREMENT for table `navbars`
+-- AUTO_INCREMENT untuk tabel `navbars`
 --
 ALTER TABLE `navbars`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pengumuman`
+-- AUTO_INCREMENT untuk tabel `pengumuman`
 --
 ALTER TABLE `pengumuman`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `prestasi`
+--
+ALTER TABLE `prestasi`
+  MODIFY `id_prestasi` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tenagapengajar`
+--
+ALTER TABLE `tenagapengajar`
+  MODIFY `id_tenagapengajar` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1241;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
