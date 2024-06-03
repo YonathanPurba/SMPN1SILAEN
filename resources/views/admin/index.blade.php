fd746e0032c10f021ce3c2bc82a0c98c90f5a928
Yonathan <yonathanpurba@yahoo.com>
2024-06-03 13:57:44 +0700

    index admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <!-- Small boxes (Stat box) -->
 <div class="row">
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-4 col-6">
         <!-- small box -->
         <div class="small-box bg-info">
             <div class="inner">
@@ -19,7 +19,7 @@
         </div>
     </div>
     <!-- ./col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-4 col-6">
         <!-- small box -->
         <div class="small-box bg-success">
             <div class="inner">
@@ -33,7 +33,7 @@
         </div>
     </div>
     <!-- ./col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-4 col-6">
         <!-- small box -->
         <div class="small-box bg-warning">
             <div class="inner">
@@ -41,13 +41,13 @@
                 <p>Pengumuman</p>
             </div>
             <div class="icon">
-                <i class="fas fa-info"></i>
+                <i class="fas fa-bullhorn"></i>
             </div>
             <a href="{{ route('admin.pengumuman.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-4 col-6">
         <!-- small box -->
         <div class="small-box bg-primary">
             <div class="inner">
@@ -55,30 +55,81 @@
                 <p>Tenaga Pengajar</p>
             </div>
             <div class="icon">
-                <i class="fas fa-chalkboard-teacher" style="color: blue;"></i> <!-- Ubah warna sesuai kebutuhan -->
+                <i class="fas fa-chalkboard-teacher"></i>
             </div>
             <a href="{{ route('admin.tenagapengajar.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
-    <div class="col-lg-3 col-6">
+    <div class="col-lg-4 col-6">
         <!-- small box -->
-        <div class="small-box bg-secondary">
+        <div class="small-box bg-danger">
             <div class="inner">
                 <h3>@count('fasilitas')</h3>
                 <p>Fasilitas</p>
             </div>
             <div class="icon">
-                <i class="fas fa-info"></i>
+                <i class="fas fa-building"></i>
             </div>
             <a href="{{ route('admin.fasilitas.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
-  </div>
-
-  <!-- ./col -->
-  
-  <!-- ./col -->
+    <!-- ./col -->
+    <div class="col-lg-4 col-6">
+        <!-- small box -->
+        <div class="small-box bg-secondary">
+            <div class="inner">
+                <h3>@count('prestasi')</h3>
+                <p>Prestasi</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-trophy"></i>
+            </div>
+            <a href="{{ route('admin.prestasi.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-4 col-6">
+        <!-- small box -->
+        <div class="small-box bg-dark">
+            <div class="inner">
+                <h3>@count('ekstrakulikuler')</h3>
+                <p>Ekstrakurikuler</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-futbol"></i>
+            </div>
+            <a href="{{ route('admin.ekstrakulikuler.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-4 col-6">
+        <!-- small box -->
+        <div class="small-box bg-light">
+            <div class="inner">
+                <h3>@count('kelas')</h3>
+                <p>Kelas</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-chalkboard"></i>
+            </div>
+            <a href="{{ route('admin.kelas.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-4 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>@count('jumlah_siswa')</h3>
+                <p>Jumlah Siswa</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-user-graduate"></i>
+            </div>
+            <a href="{{ route('admin.jumlah_siswa.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
 </div>
 <!-- /.row -->
 @stop
--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,9 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Admin\ProfileController;
 use App\Http\Controllers\ChangePasswordController;
 
-
 //Controllers Namespace
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfilController;
-use App\Http\Controllers\ArtikelController;
 use App\Http\Controllers\EkstrakulikulerController;
 use App\Http\Controllers\KelasController;
 use App\Http\Controllers\PengumumanController;
@@ -20,7 +18,6 @@ use App\Http\Controllers\PrestasiController;
 use App\Http\Controllers\FasilitasController;
 use App\Http\Controllers\GaleriController;
 use App\Http\Controllers\TenagaPengajarController;
-use App\Models\TenagaPengajar;
 
 /*
 |--------------------------------------------------------------------------
@@ -52,7 +49,6 @@ Route::get('/tenagapengajar/{tenagapengajar:slug}',[TenagaPengajarController::cl
 Route::get('/prestasi', [PrestasiController::class,'view'])->name('prestasi.index');
 Route::get('/prestasi/{prestasi:slug}',[PrestasiController::class,'show'])->name('prestasi.show');
 
-
 //Fasilitas
 Route::get('/fasilitas', [FasilitasController::class,'view'])->name('fasilitas.index');
 Route::get('/fasilitas/{fasilitas:slug}',[FasilitasController::class,'show'])->name('fasilitas.show');
@@ -60,8 +56,6 @@ Route::get('/fasilitas/{fasilitas:slug}',[FasilitasController::class,'show'])->n
 //Pengumuman
 Route::get('/pengumuman', [PengumumanController::class,'view'])->name('pengumuman.index');
 Route::get('/pengumuman/{pengumuman:slug}',[PengumumanController::class,'show'])->name('pengumuman.show');
-
-
 
 //Profil
 Route::get('/visimisi',[ProfilController::class,'visimisi'])->name('visimisi');
@@ -80,7 +74,6 @@ Route::group(['prefix' => 'admin','middleware' => ['auth']],function(){
 		Route::get('/fasilitas', [FasilitasController::class,'index'])->name('fasilitas.index');
 		Route::get('/fasilitas/create', [FasilitasController::class, 'create'])->name('fasilitas.create');
 		Route::post('/fasilitas', [FasilitasController::class, 'store'])->name('fasilitas.store');
-		// Route::get('/fasilitas/index/delete/{id}', [FasilitasController::class, 'delete']);
 		Route::get('/fasilitas/edit/{id}', [FasilitasController::class, 'edit'])->name('fasilitas.edit');
 		Route::put('/fasilitas/edit/{id}', [FasilitasController::class, 'update'])->name('fasilitas.edit.update');
 		Route::delete('fasilitas/{id}/delete', [FasilitasController::class, 'delete'])->name('fasilitas.index.delete');
@@ -130,7 +123,6 @@ Route::group(['prefix' => 'admin','middleware' => ['auth']],function(){
 		//Resource Controller
 		Route::resource('users','UsersController');
 		Route::resource('pengumuman','PengumumanController');
-		// Route::resource('artikel','ArtikelController');
 		Route::resource('galeri','GaleriController');
 		Route::resource('kategori-artikel','KategoriArtikelController');
 
