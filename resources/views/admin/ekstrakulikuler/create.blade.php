63964de27c7518c09bb29c5ae500e19e090616a4
Andika PT <andikatampubolon72@gmail.com>
2024-06-04 20:25:37 +0700

    terbaru

--- resources/views/admin/ekstrakulikuler/create.blade.php
+++ resources/views/admin/ekstrakulikuler/create.blade.php
@@ -3,6 +3,10 @@
 	'contentTitle' => 'Tambah Ekstrakulikuler',
 ])
 @section('content')
+@push('css')
+<link rel="stylesheet" type="text/css" href="{{ asset('plugins/summernote') }}/summernote-bs4.min.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('plugins/dropify') }}/dist/css/dropify.min.css">
+@endpush
 <div class="row">
 	<div class="col">
 		<div class="card">
@@ -17,13 +21,15 @@
 						<input required="" class="form-control" type="" name="judul_ekstrakulikuler" id="judul_ekstrakulikuler" placeholder="">
 					</div>
 					<div class="form-group">
-						<label for="deskripsi_ekstrakulikuler">Deskripsi</label>
-						<textarea required="" name="deskripsi_ekstrakulikuler" id="deskripsi_ekstrakulikuler" class="text-dark form-control summernote"></textarea>
+						<label for="deskripsi">Deskripsi</label>
+						<textarea required="" name="deskripsi_ekstrakulikuler" id="deskripsi" class="text-dark form-control summernote"></textarea>
 					</div>
-                    <div class="col-lg-6">
-                        <div class="form-group">
-                            <label>Gambar</label>
-                            <input type="file" name="gambar_ekstrakulikuler" class="dropify form-control" data-height="190" data-allowed-file-extensions="png jpg gif jpeg svg webp jfif" required>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label>Gambar</label>
+                                <input type="file" name="gambar_ekstrakulikuler" class="dropify form-control" data-height="190" data-allowed-file-extensions="png jpg gif jpeg svg webp jfif" required>
+                            </div>
                         </div>
                     </div>
 					<div class="form-group">
@@ -35,3 +41,40 @@
 	</div>
 </div>
 @stop
+@push('js')
+<script type="text/javascript" src="{{ asset('plugins/summernote') }}/summernote-bs4.min.js"></script>
+<script type="text/javascript" src="{{ asset('plugins/dropify') }}/dist/js/dropify.min.js"></script>
+<script type="text/javascript">
+    $(".summernote").summernote({
+        height:500,
+        callbacks: {
+        // callback for pasting text only (no formatting)
+            onPaste: function (e) {
+              var bufferText = ((e.originalEvent || e).clipboardData || window.clipboardData).getData('Text');
+              e.preventDefault();
+              bufferText = bufferText.replace(/\r?\n/g, '<br>');
+              document.execCommand('insertHtml', false, bufferText);
+            }
+        }
+    })
+
+    $(".summernote").on("summernote.enter", function(we, e) {
+        $(this).summernote("pasteHTML", "<br><br>");
+        e.preventDefault();
+    });
+
+	$('.dropify').dropify({
+        messages: {
+            default: 'Drag atau Drop untuk memilih gambar',
+            replace: 'Ganti',
+            remove:  'Hapus',
+            error:   'error'
+        }
+    });
+
+    $('.title').keyup(function(){
+        var title = $(this).val().toLowerCase().replace(/[&\/\\#^, +()$~%.'":*?<>{}]/g,'-');
+        $('.slug').val(title);
+    });
+</script>
+@endpush
