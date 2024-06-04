6c5cdcbb20a9d6d9edb1479c3c4310485cd570c4
Andika PT <andikatampubolon72@gmail.com>
2024-06-04 14:47:08 +0700

    terbaru

---
+++ app/Http/Controllers/KepalaSekolahController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\KepalaSekolah;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\View;
+
+class KepalaSekolahController extends Controller
+{
+    public function index()
+    {
+        $kepalasekolah = KepalaSekolah::all();
+        return view('admin.kepalasekolah.index',compact('kepalasekolah'));
+    }
+
+    public function store(Request $request)
+    {
+        $kepalasekolah = new KepalaSekolah();
+        $kepalasekolah->nama = $request->nama;
+        $kepalasekolah->nip = $request->nip;
+        $kepalasekolah->created_by = Auth::id();
+        $kepalasekolah->update_by = Auth::id();
+    
+            if ($request->hasFile('gambar_kepalasekolah')) {
+                $file = $request->file('gambar_kepalasekolah');
+                $filename = $file->getClientOriginalName();
+                $file->move(public_path('folderimage'), $filename);
+                $kepalasekolah->gambar_kepalasekolah = $filename;
+            } else {
+                $kepalasekolah->gambar_kepalasekolah = null;
+            }
+    
+        $kepalasekolah->save();
+    
+        return redirect()->route('admin.kepalasekolah.index')->with('success', 'Kepala Sekolah berhasil ditambahkan.');
+}
+    
+    public function create()
+    {
+        return view('admin.kepalasekolah.create');
+    }
+    public function delete($id)
+{
+    $kepalasekolah = KepalaSekolah::find($id);
+
+    if ($kepalasekolah) {
+        $kepalasekolah->delete();
+        return response()->json(['success' => 'Data berhasil dihapus!']);
+    }
+
+    return response()->json(['error' => 'Data tidak ditemukan atau gagal dihapus!'], 404);
+}
+
+
+    public function update(Request $request, $id)
+{
+    $update = KepalaSekolah::find($id);
+
+    // Jika ada file gambar yang diunggah
+    if ($request->hasFile('gambar_kepalasekolah')) {
+        $file = $request->file('gambar_kepalasekolah');
+        $filename = $file->getClientOriginalName();
+        $file->move(public_path('folderimage'), $filename);
+        $update->gambar_kepalasekolah = $filename;
+    }
+
+    // Update data lainnya
+    $update->nama = $request->nama;
+    $update->nip = $request->nip;
+    $update->update_by = Auth::id();
+
+    // Simpan perubahan ke dalam database
+    $update->save();
+
+    // Redirect ke halaman daftar kategori lapangan
+    return redirect()->route('admin.kepalasekolah.index')->with('success', 'Kepala Sekolah berhasil diedit.');
+    }
+    public function edit($id)
+    {
+        $kepalasekolah = KepalaSekolah::find($id);
+        return view('admin.kepalasekolah.edit', compact('kepalasekolah'));
+    }
+}
